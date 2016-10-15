cde45de04fb8f796d5f6e671b04cdd29eda34641
Marcel <MTRNord@users.noreply.github.com>
2016-10-15 22:02:15 +0200

    Update Roads.sh

--- Roads.sh
+++ Roads.sh
@@ -2,7 +2,7 @@
 cd ImageNet_Utils
 ./downloadutils.py --downloadImages --wnid n03215508
 ./downloadutils.py --downloadImages --wnid n02744323
-mkdir -p ../road_data/road
-mv ./n03215508/url_images/* ../road_data/road/*
-mv ./n02744323/url_images/* ../road_data/road/*
+mkdir -p ../road_data/data
+mv ./n03215508/url_images/* ../road_data/data/*
+mv ./n02744323/url_images/* ../road_data/data/*
 cd ..
